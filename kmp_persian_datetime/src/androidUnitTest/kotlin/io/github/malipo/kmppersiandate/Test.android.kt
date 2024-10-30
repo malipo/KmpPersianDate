6d526c7dcf2ccedfb627e2bc2cae5cca51952c47
malihe.pooraskari <malihe.pooraskari@gmail.com>
2024-10-30 17:05:52 +0330

    Initial commit

---
+++ kmp_persian_datetime/src/androidUnitTest/kotlin/io/github/malipo/kmppersiandate/Test.android.kt
@@ -0,0 +1,12 @@
+package io.github.malipo.kmppersiandate
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+    }
+}
