4d100b2408fd8ea29ce7fbe712468804110884cc
Malihe <malihe.pooraskari@gmail.com>
2024-10-31 12:46:56 +0330

    add font and style

---
+++ androidAppSample/src/main/java/io/github/malipo/kmppersiandate/android/theme/TextStyle.kt
@@ -0,0 +1,17 @@
+package io.github.malipo.kmppersiandate.android.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.sp
+import io.github.malipo.kmppersiandate.android.R
+
+val title_Style = TextStyle(
+    fontFamily = FontFamily(Font(R.font.medium)),
+    fontSize = 16.sp,
+    color = Color.Black,
+    textDirection = TextDirection.Rtl,
+)
